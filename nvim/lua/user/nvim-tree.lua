02727e45f479c572d7546f0d204ad9576a1afda2
christopher320n <christophera320n@gmail.com>
2024-11-01 21:09:48 +0100

    nvim-tree C-k popup now also has border

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -13,6 +13,13 @@ require "nvim-tree".setup {
   modified = {
     enable = true,
   },
+  actions = {
+    file_popup = {
+      open_win_config = {
+        border = "rounded",
+      }
+    }
+  },
   renderer = {
     highlight_opened_files = "name",
     root_folder_label = false,
@@ -32,3 +39,9 @@ vim.opt.fillchars = { eob = " "}
 vim.cmd('hi NvimTreeWinSeparator guifg=#FFFFFF guibg=NONE')
 vim.cmd('hi WinSeparator guifg=#FFFFFF guibg=NONE')
 
+
+vim.ui.select = function(items, opts, on_choice)
+  opts = opts or {}
+  opts.border = "rounded"  -- Set tooltip border style here: "single", "double", "rounded", etc.
+  vim.ui._default_select(items, opts, on_choice)
+end
