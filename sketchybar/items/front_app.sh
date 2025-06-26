e8bbe38842eb4eb614997396e77a842b5ffbf1ec
christopher320n <christophera320n@gmail.com>
2025-06-26 20:06:53 +0200

    Added padding to the right of front app component

--- sketchybar/items/front_app.sh
+++ sketchybar/items/front_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sketchybar --add item front_app left \
-    --set front_app icon.drawing=off label.padding_left=14 label.max_chars=14 scroll_texts=on script="$PLUGIN_DIR/front_app.sh" \
+    --set front_app icon.drawing=off label.padding_left=14 label.max_chars=14 scroll_texts=on padding_right=5 script="$PLUGIN_DIR/front_app.sh" \
     click_script="${CONFIG_DIR}/helper/open-app.sh" \
     --subscribe front_app front_app_switched
