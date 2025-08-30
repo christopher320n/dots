e23998e2751e72de6956e17d42940e70624f6b62
christopher320n <christophera320n@gmail.com>
2025-08-30 18:40:56 +0200

    Removed unused configurations

--- sketchybar/items/date.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sketchybar --add item date right
-sketchybar --set date update_freq=1 script="${CONFIG_DIR}/plugins/date.sh" \
-                 padding_right=-14 \
-                 click_script="open -a Calendar.app"
-
--- sketchybar/items/time.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sketchybar --add item time right
-sketchybar --set time update_freq=60 script="${CONFIG_DIR}/plugins/time.sh" \
-             click_script="open x-apple.systempreferences:com.apple.preference.datetime"
-
