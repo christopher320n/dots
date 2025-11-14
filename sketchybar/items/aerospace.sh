28e973045a4723b7c4b9cc5dd3fcfef60475e14f
christopher320n <christophera320n@gmail.com>
2025-11-14 18:38:35 +0100

    Configuring sketchybar

--- sketchybar/items/aerospace.sh
+++ sketchybar/items/aerospace.sh
@@ -3,16 +3,10 @@
 sketchybar --add event aerospace_workspace_change
 sketchybar --add event aerospace_monitor_change
 
-for sid in $(aerospace list-workspaces --all); do
+for sid in $(aerospace list-workspaces --focused); do
   sketchybar --add item space.$sid left \
     --subscribe space.$sid aerospace_workspace_change \
     --set space.$sid \
-    display="$(
-      v=$(aerospace list-windows --workspace "$sid" --format "%{monitor-appkit-nsscreen-screens-id}" | cut -c1)
-      echo "${v:-1}"
-    )" \
-    background.corner_radius=5 \
-    background.border_width=0 \
     icon="$sid" \
     label.font="sketchybar-app-font:Regular:16.0" \
     click_script="aerospace workspace $sid" \
--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -7,8 +7,10 @@ source "${CONFIG_DIR}/themes/e-ink.sh"
 # make sure it's executable with:
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME icon.color=${COLOUR_TEXT}
-else
-  sketchybar --set $NAME icon.color=none
-fi
+sketchybar --add event aerospace_workspace_change
+sketchybar --add event aerospace_monitor_change
+
+
+for sid in $(aerospace list-workspaces --focused); do
+  sketchybar --set $NAME icon="$sid" icon.color=${COLOUR_TEXT}
+done
