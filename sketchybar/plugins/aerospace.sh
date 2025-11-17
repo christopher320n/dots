b4caeb03cb7576fd037e59f7de3d6c2d6496f855
christopher320n <christophera320n@gmail.com>
2025-11-17 21:05:19 +0100

    removed underscore on aerospace, streamlined aerospace gap

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -12,13 +12,13 @@ sketchybar --add event aerospace_monitor_change
 
 for sid in $(aerospace list-workspaces --focused); do
   sketchybar --set $NAME \
-             icon="$sid" \
+             icon="A-$sid" \
              icon.color=${COLOUR_TEXT} \
-             background.color=${COLOUR_TEXT} \
+             #background.color=${COLOUR_TEXT} \
              background.height=2 \
              background.y_offset=-10 \
              background.corner_radius=10 \
+             background.x_offset=-7 \
              icon.padding_left=-5 \
-             icon.padding_right=-5 \
-             background.x_offset=-7
+             icon.padding_right=-5
 done
